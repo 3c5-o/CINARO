440c74cc5da92a471c3093cd3acc619f548bffbe
3c5-o <ffkyyr@gmail.com>
2026-09-24 11:28:56 +0300

    chore(web): align Firebase analytics with 2.4.0

--- web/firebase.js
+++ web/firebase.js
@@ -190,7 +190,7 @@ async function bootFirebase() {
     analyticsSdk.isSupported().then(function (supported) {
       if (!supported) return;
       analytics = analyticsSdk.getAnalytics(app);
-      analyticsSdk.logEvent(analytics, "app_open", { app_version: "2.3.2" });
+      analyticsSdk.logEvent(analytics, "app_open", { app_version: "2.4.0" });
     }).catch(function () {});
   }
 
